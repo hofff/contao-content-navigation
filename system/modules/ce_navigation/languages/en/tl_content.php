34818261d2ad1db75be3f1ef1b6d49c9ca9c9dd4
Tristan Lins <tristan.lins@infinitysoft.de>
2010-11-23 13:13:08 +0000

    Adding english language.

---
+++ system/modules/ce_navigation/languages/en/tl_content.php
@@ -0,0 +1,40 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  InfinitySoft 2010
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    Language
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['navigation_article']        = array('Artikel', 'Artikel auswählen, über den die Inhaltsnavigation erstellt werden soll.');
+$GLOBALS['TL_LANG']['tl_content']['navigation_min_level']      = array('Start Level', 'Bei dieser Überschriftenebene anfangen (inklusive).');
+$GLOBALS['TL_LANG']['tl_content']['navigation_max_level']      = array('Stopp Level', 'Bei dieser Überschriftenebene aufhören (inklusive).');
+$GLOBALS['TL_LANG']['tl_content']['navigation_article_column'] = 'Artikel in Spalte';
+$GLOBALS['TL_LANG']['tl_content']['navigation_article_page']   = 'Artikel in Seite';
+
+?>
