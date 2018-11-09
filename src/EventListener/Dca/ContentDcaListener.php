a16fc5295c2e4332f2d8ad47dcfa2b4f7b93f3ce
David Molineus <david.molineus@netzmacht.de>
2018-11-09 14:21:11 +0100

    Limit sections to active used sections.

--- src/EventListener/Dca/ContentDcaListener.php
+++ src/EventListener/Dca/ContentDcaListener.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Hofff\Contao\ContentNavigation\EventListener\Dca;
 
+use Contao\Backend;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
-use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Contao\DataContainer;
+use Contao\LayoutModel;
 use Contao\StringUtil;
 use Doctrine\DBAL\Connection;
+use Hofff\Contao\ContentNavigation\Navigation\Query\ArticlePageQuery;
 use function is_array;
 use Patchwork\Utf8;
 use PDO;
@@ -23,22 +25,20 @@ final class ContentDcaListener
     private $connection;
 
     /**
-     * Contao framework.
-     *
-     * @var ContaoFrameworkInterface
+     * @var ArticlePageQuery
      */
-    private $framework;
+    private $articlePageQuery;
 
     /**
      * ContentDcaListener constructor.
      *
-     * @param Connection               $connection
-     * @param ContaoFrameworkInterface $framework
+     * @param Connection       $connection
+     * @param ArticlePageQuery $articlePageQuery
      */
-    public function __construct(Connection $connection, ContaoFrameworkInterface $framework)
+    public function __construct(Connection $connection, ArticlePageQuery $articlePageQuery)
     {
-        $this->connection = $connection;
-        $this->framework  = $framework;
+        $this->connection       = $connection;
+        $this->articlePageQuery = $articlePageQuery;
     }
 
     public function adjustPalettes(): void
@@ -76,47 +76,13 @@ final class ContentDcaListener
             return [];
         }
 
-        $columns = [];
-        foreach (['header', 'left', 'main', 'right', 'footer'] as $column) {
-            $columns[$column] = $GLOBALS['TL_LANG']['COLS'][$column] ?? $column;
-        }
-
-        $articles  = [];
-        $statement = $this->connection->prepare(
-            '
-			SELECT
-				a.id,
-				a.title,
-				a.inColumn
-			FROM
-				tl_article a
-			INNER JOIN
-				tl_article b
-				ON a.pid = b.pid
-			INNER JOIN
-				tl_content c
-				ON c.pid = b.id
-			WHERE
-				c.id = :id
-			ORDER BY
-				a.inColumn,
-				a.sorting'
-        );
-
-        $statement->bindValue('id', $dataContainer->id);
-        $statement->execute();
-
-        while ($row = $statement->fetch(PDO::FETCH_OBJ)) {
-            $articles[$row->id] = sprintf(
-                '%s [%s]',
-                $row->title,
-                $GLOBALS['TL_LANG']['COLS'][$row->inColumn] ?? $row->inColumn
-            );
-        }
-
         return [
-            $GLOBALS['TL_LANG']['tl_content']['hofff_toc_source_column'] => $columns,
-            $GLOBALS['TL_LANG']['tl_content']['hofff_toc_source_page']   => $articles,
+            $GLOBALS['TL_LANG']['tl_content']['hofff_toc_source_column'] => $this->activeSections(
+                (int) $dataContainer->activeRecord->pid
+            ),
+            $GLOBALS['TL_LANG']['tl_content']['hofff_toc_source_page']   => $this->pageArticles(
+                (int) $dataContainer->id
+            )
         ];
     }
 
@@ -152,5 +118,101 @@ final class ContentDcaListener
         $value[0] = Utf8::strtolower(trim($cssId, '-'));
 
         return $value;
+    }
+
+    private function activeSections(int $articleId): array
+    {
+        // Show only active sections
+        if ($articleId) {
+            $sections = [];
+            $page     = ($this->articlePageQuery)($articleId);
+
+            if ($page) {
+                $page->loadDetails();
+
+                // Get the layout sections
+                foreach (['layout', 'mobileLayout'] as $key) {
+                    if (!$page->$key) {
+                        continue;
+                    }
+
+                    $layout = LayoutModel::findByPk($page->$key);
+
+                    if ($layout === null) {
+                        continue;
+                    }
+
+                    $modules = StringUtil::deserialize($layout->modules);
+                    if (empty($modules) || !\is_array($modules)) {
+                        continue;
+                    }
+
+                    // Find all sections with an article module (see #6094)
+                    foreach ($modules as $module) {
+                        if ($module['mod'] == 0 && $module['enable']) {
+                            $sections[] = $module['col'];
+                        }
+                    }
+                }
+            }
+        } else {
+            // Show all sections (e.g. "override all" mode)
+
+            $sections = ['header', 'left', 'right', 'main', 'footer'];
+            $statement   = $this->connection->executeQuery('SELECT sections FROM tl_layout WHERE sections!=\'\'');
+
+            while ($layout = $statement->fetch(PDO::FETCH_OBJ)) {
+                $arrCustom = StringUtil::deserialize($layout->sections);
+
+                // Add the custom layout sections
+                if (!empty($arrCustom) && \is_array($arrCustom)) {
+                    foreach ($arrCustom as $v) {
+                        if (!empty($v['id'])) {
+                            $sections[] = $v['id'];
+                        }
+                    }
+                }
+            }
+        }
+
+        return Backend::convertLayoutSectionIdsToAssociativeArray($sections);
+    }
+
+    private function pageArticles(int $contentId): array
+    {
+        $articles  = [];
+        $statement = $this->connection->prepare(
+            '
+			SELECT
+				a.id,
+				a.title,
+				a.inColumn
+			FROM
+				tl_article a
+			INNER JOIN
+				tl_article b
+				ON a.pid = b.pid
+			INNER JOIN
+				tl_content c
+				ON c.pid = b.id
+			WHERE
+				c.id = :id
+			ORDER BY
+				a.inColumn,
+				a.sorting'
+        );
+
+        $statement->bindValue('id', $contentId);
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_OBJ)) {
+            $articles[$row->id] = sprintf(
+                '%s [%s]',
+                $row->title,
+                $GLOBALS['TL_LANG']['COLS'][$row->inColumn] ?? $row->inColumn
+            );
+        }
+
+        return $articles;
     }
 }
